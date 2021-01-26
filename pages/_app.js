d2e6d63cb3d138cf3a5ebedf6265d4f4445de575
bmansk8 <bmansk8@yahoo.com>
2021-01-26 11:21:47 -0500

    first push
    Completed the bulk of the code. Will add more articles, photos, and music. Also need to get some hosting for it.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+import '../public/css/article.scss'
+import '../public/css/gallery.scss'
+import '../public/css/song.scss'
+import '../public/css/main.scss'
+import '../styles/globals.scss'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/posts/photo_fog.jsx
@@ -0,0 +1,30 @@
+export default function Photo_fog(params) {
+  return (
+    <div className="gallery">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nothing+You+Could+Do&display=swap"
+        rel="stylesheet"
+      />
+      <h1>the fog collection</h1>
+      <img src="/coverArt1.jpg" alt="photo in gallery" />
+      <img src="/coverArt2.jpg" alt="photo in gallery" />
+      <img src="/coverArt3.jpg" alt="photo in gallery" />
+      <img src="/coverArt4.jpg" alt="photo in gallery" />
+      <img src="/coverArt5.jpg" alt="photo in gallery" />
+      <img src="/coverArt6.jpg" alt="photo in gallery" />
+      <p>
+        <em>
+          <span className="firstW">So,</span>
+        </em>{" "}
+        this collection is from three separate occasions. The first photo (top
+        left) was on my daily run. It happened to be foggy that day, normally
+        that part of the lake looks really ugly, so for such a great photo to
+        come of it is pretty rare. The second one was in the midst of a
+        recording session. It’s taken from within the studio. The rest were on
+        an amazing hiking session around a reservoir near my house. Funny enough
+        these are all on different days even thought the fog is somewhat
+        consistent between them all.
+      </p>
+    </div>
+  );
+}
